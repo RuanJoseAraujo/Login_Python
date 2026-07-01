e65d6537248b0872393a0f8cc5bbb29153d7344c
𝘙𝘶𝘢𝘯 𝘑𝘰se <ruangodoyalves@gmail.com>
2026-06-30 21:03:04 -0300

    Starting the database and the system file

---
+++ database.py
@@ -0,0 +1,11 @@
+import sqlite3 
+
+banco = sqlite3.connect('login.db')
+
+cursor = banco.cursor()
+
+# cursor.execute("CREATE TABLE users (id integer, name text, birthdate text, email text)")
+
+cursor.execute("select * from users")
+
+print(cursor.fetchall())
